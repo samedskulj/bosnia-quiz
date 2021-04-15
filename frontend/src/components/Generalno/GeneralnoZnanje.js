d41937a3ea782a7f82f752d7317293710abda309
Škulj Samed <samed.skulj08@gmail.com>
2021-04-15 18:02:27 +0200

    Generalno znanje gotovo

--- frontend/src/components/Generalno/GeneralnoZnanje.js
+++ frontend/src/components/Generalno/GeneralnoZnanje.js
@@ -4,9 +4,12 @@ import GeneralnoZnanjeIgra from "./GeneralnoZnanjeIgra";
 export const mjenjanjeIgre = createContext();
 const GeneralnoZnanje = () => {
   const [gameState, setGameState] = useState("meni");
+  const [odgovori, setOdgovori] = useState([]);
   return (
     <>
-      <mjenjanjeIgre.Provider value={{ gameState, setGameState }}>
+      <mjenjanjeIgre.Provider
+        value={{ gameState, setGameState, odgovori, setOdgovori }}
+      >
         {gameState === "meni" && <GeneralnoZnanjeMeni></GeneralnoZnanjeMeni>}
         {gameState === "igra" && <GeneralnoZnanjeIgra></GeneralnoZnanjeIgra>}
       </mjenjanjeIgre.Provider>
--- frontend/src/components/Generalno/GeneralnoZnanjeIgra.js
+++ frontend/src/components/Generalno/GeneralnoZnanjeIgra.js
@@ -1,31 +1,61 @@
 import { MDBBtn } from "mdb-react-ui-kit";
-import React, { useState, useContext } from "react";
+import React, { useState, useContext, useEffect, useRef } from "react";
 import { connect } from "react-redux";
 import { mjenjanjeIgre } from "./GeneralnoZnanje";
 import "./GeneralnoZnanjeIgra.css";
 const GeneralnoZnanjeIgra = ({ pitanjeZnanje }) => {
   const [trenutnoPitanje, setTrenutnoPitanje] = useState(0);
-  const { gameState, setGameState } = useContext(mjenjanjeIgre);
+  const [odabran, setOdabran] = useState("");
+  const radiow = useRef();
+
+  const odabranHandler = (e) => {
+    setOdabran(e.target.value);
+  };
+
+  useEffect(() => {
+    const radiobox = radiow.current.querySelector("input:checked");
+    if (radiobox) {
+      radiobox.checked = false;
+    }
+  }, [trenutnoPitanje]);
+
+  const { gameState, setGameState, odgovori, setOdgovori } = useContext(
+    mjenjanjeIgre
+  );
+
   const trenutnoPitanjeHandler = () => {
     if (trenutnoPitanje !== pitanjeZnanje.length - 1) {
       setTrenutnoPitanje(trenutnoPitanje + 1);
+      setOdgovori((prev) => [
+        ...prev,
+        { pitanje: pitanjeZnanje[trenutnoPitanje].pitanje, odgovor: odabran },
+      ]);
+      setOdabran("");
     } else {
       setGameState("meni");
     }
   };
-  console.log(pitanjeZnanje);
+  console.log(odgovori);
+
   return (
     <>
       <main className="generalnoznanjeigra">
         <div className="generalnoznanjeigra_div">
           {pitanjeZnanje[trenutnoPitanje].pitanje}
-          {pitanjeZnanje[trenutnoPitanje].izbori.map((odabir, i) => {
-            return (
-              <div className="izbori" key={i}>
-                <p>{odabir}</p>
-              </div>
-            );
-          })}
+          <div className="izbori" ref={radiow}>
+            {pitanjeZnanje[trenutnoPitanje].izbori.map((odabir, i) => (
+              <label htmlFor="odabir" key={i}>
+                <input
+                  id="test"
+                  type="radio"
+                  name="odabir"
+                  value={odabir}
+                  onChange={odabranHandler}
+                />
+                {odabir}
+              </label>
+            ))}
+          </div>
           <MDBBtn color="dark" onClick={trenutnoPitanjeHandler}>
             Potvrdi
           </MDBBtn>
